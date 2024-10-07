b44e7233076467d9a40f9649d957a40c268045f4
Sergii Lysenko <soulwish.ls@gmail.com>
2024-10-06 21:14:44 -0400

    add footer and header sections

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,10 @@
+<footer>
+	<div class="container">
+		<div class="flex items-center justify-between gap-8 py-7">
+			<img src="/images/agrarium-logo.svg" alt="Agrarium" width="52" height="33" />
+			<p class="">
+				Copyright © {new Date().getFullYear()} Agrarium.io All rights reserved.
+			</p>
+		</div>
+	</div>
+</footer>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,15 @@
+<header>
+	<div class="container">
+		<div
+			class="flex flex-col items-center justify-start gap-6 pb-8 pt-8 md:flex-row md:gap-14 md:pb-24"
+		>
+			<img src="/images/agrarium-logo-full.svg" alt="Agrarium" width="206" height="33" />
+			<nav class="flex gap-4 font-futura">
+				<a href="/" class="underline-offset-4 hover:underline">Explore</a>
+				<a href="/" class="underline-offset-4 hover:underline">Features</a>
+				<a href="/" class="underline-offset-4 hover:underline">How it works</a>
+				<a href="/" class="underline-offset-4 hover:underline">FAQ</a>
+			</nav>
+		</div>
+	</div>
+</header>
