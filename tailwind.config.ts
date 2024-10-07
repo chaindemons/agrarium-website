c88d57885d2abe43d2aeafe7399047d0bd4f0859
Sergii Lysenko <soulwish.ls@gmail.com>
2024-10-06 21:16:03 -0400

    configure tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,30 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			container: {
+				center: true, // Centers the container
+				padding: {
+					DEFAULT: '1rem', // 1rem padding on all screen sizes
+					sm: '2rem' // 2rem padding on small screens
+				},
+				screens: {
+					sm: '640px',
+					md: '768px',
+					lg: '1024px',
+					xl: '1280px'
+				}
+			},
+			fontFamily: {
+				futura: ['Futura', 'sans-serif'],
+				fustat: ['Fustat', 'sans-serif']
+			},
+			colors: {
+				ashgray: '#CAD2C5',
+				black: '#000',
+				white: '#FFF'
+			}
+		}
 	},
 
 	plugins: []
