4085a91614110787c84ad4472a105d8d2a913ba3
chaindemons <chaindemonsco@gmail.com>
2024-10-08 22:53:51 -0400

    add follow us button in header section

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,15 +1,20 @@
 <header>
 	<div class="container">
 		<div
-			class="flex flex-col items-center justify-start gap-6 pb-8 pt-8 md:flex-row md:gap-14 md:pb-24"
+			class="flex flex-col items-center justify-start gap-6 pb-8 pt-8 md:flex-row md:gap-10 md:pb-24 xl:gap-14"
 		>
 			<img src="/images/agrarium-logo-full.svg" alt="Agrarium" width="206" height="33" />
-			<nav class="flex gap-4 font-futura">
+			<nav class="flex gap-4 whitespace-nowrap font-futura">
 				<a href="#explore" class="underline-offset-4 hover:underline">Explore</a>
 				<a href="#features" class="underline-offset-4 hover:underline">Features</a>
 				<a href="#how-it-works" class="underline-offset-4 hover:underline">How it works</a>
 				<a href="#faq" class="underline-offset-4 hover:underline">FAQ</a>
 			</nav>
+			<a
+				href="https://x.com/agrariumio"
+				class="whitespace-nowrap rounded-xl border border-black px-5 py-2 font-futura hover:bg-black hover:text-white md:ml-auto"
+				target="_blank">Follow us</a
+			>
 		</div>
 	</div>
 </header>
